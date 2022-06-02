8b23a332729700620b65b4b65b861aaffa103672
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-02 10:34:00 +0600

    completed create porst and render post section

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import Posts from "../Posts/Posts";
+import PostShare from "../PostShare/PostShare";
+import "./PostSide.css";
 const PostSide = () => {
   return (
-    <div>PostSide</div>
-  )
-}
+    <div className="PostSide">
+      <PostShare />
+      <Posts />
+    </div>
+  );
+};
 
-export default PostSide
+export default PostSide;
