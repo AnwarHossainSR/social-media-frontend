2f3d25c788ad65072e346bd877138c192c09024f
Anwar Hossain <anwar.sarker@bjitgroup.com>
2022-06-02 10:22:00 +0600

    completed Right side

---
+++ src/components/PostShare/PostShare.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostShare = () => {
+  return <div>PostShare</div>;
+};
+
+export default PostShare;
--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -3,9 +3,9 @@ import "./RightSide.css";
 import Home from "../../img/home.png";
 import Noti from "../../img/noti.png";
 import Comment from "../../img/comment.png";
-//import { UilSetting } from "@iconscout/react-unicons";
-//import TrendCard from "../TrendCard/TrendCard";
-//import ShareModal from "../ShareModal/ShareModal";
+import { UilSetting } from "@iconscout/react-unicons";
+import TrendCard from "../TrendCard/TrendCard";
+import ShareModal from "../ShareModal/ShareModal";
 
 const RightSide = () => {
   const [modalOpened, setModalOpened] = useState(false);
@@ -13,17 +13,17 @@ const RightSide = () => {
     <div className="RightSide">
       <div className="navIcons">
         <img src={Home} alt="" />
-        {/* <UilSetting /> */}
+        <UilSetting />
         <img src={Noti} alt="" />
         <img src={Comment} alt="" />
       </div>
 
-      {/* <TrendCard /> */}
+      <TrendCard />
 
       <button className="button r-button" onClick={() => setModalOpened(true)}>
         Share
       </button>
-      {/* <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} /> */}
+      <ShareModal modalOpened={modalOpened} setModalOpened={setModalOpened} />
     </div>
   );
 };
